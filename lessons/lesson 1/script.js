65bc3db361564611d97347299ec7b620ba1ed46d
Ivan Gavrilov <gavrilovivan2000@gmail.com>
2024-02-24 18:08:37 +0100

    Initilaize project

---
+++ lessons/lesson 1/script.js
@@ -0,0 +1,47 @@
+"use strict";
+
+// console.log("Текст из script.js");
+
+// const num = 123;
+// console.log(num);
+
+// let a = 5;
+// a = 7;
+// console.log(a);
+
+// const b = 1 + 2 + 3;
+// console.log(b);
+
+// short comment
+
+/*
+long comment
+*/
+
+//////////
+
+// const myAge = 20;
+// const thisYear = 2024;
+
+// const sum = thisYear + myAge;
+// const dif = thisYear - myAge;
+// const mul = thisYear * myAge;
+// const div = thisYear / myAge;
+
+// console.log(sum, dif, mul, div);
+// console.log(sum + dif + mul + div);
+
+//////////
+
+const a = 0.1;
+const b = 0.2;
+console.log(a + b);
+const c = 123.54;
+
+console.log(Math.round(c));
+console.log(Math.floor(c));
+console.log(Math.ceil(c));
+console.log(Math.trunc(c));
+
+const d = -77;
+console.log(-d);
